702a337a30e445a18eb9971d85c55809990a3c8f
Seafoamspace <105249804+Seafoamspace@users.noreply.github.com>
2022-07-07 18:30:38 -0400

    added low temp for  today

--- src/script.js
+++ src/script.js
@@ -73,6 +73,8 @@ function displayWeather(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#feels").innerHTML = Math.round(response.data.main.feels_like);
+  document.querySelector("#mint").innerHTML = Math.round(response.data.main.temp_min);
+
   document.querySelector("#descript").innerHTML =
   response.data.weather[0].description;
   document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
